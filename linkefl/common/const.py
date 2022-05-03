f0f9360e8a54b7dce017727ce6f316785cc44e49
tanjuntao <iamtanjuntao@gmail.com>
2022-05-03 10:49:55 +0800

    put global constant variables in a single file

---
+++ linkefl/common/const.py
@@ -0,0 +1,26 @@
+"""Store global constant variables"""
+
+
+class Const:
+    ACTIVE_NAME = 'active_party'
+    PASSIVE_NAME = 'passive_party'
+
+    RANDOM = 'random'
+    SEQUENCE = 'sequence'
+    IMPORTANCE = 'importance'
+
+    START_SIGNAL = 'start'
+
+    BUILDIN_DATASET = ['cancer', 'digits', 'epsilon', 'census', 'credit']
+
+    PLAIN = 'plain'
+    PAILLIER = 'paillier'
+    FAST_PAILLIER = 'fast_paillier'
+
+    L1 = 'l1'
+    L2 = 'l2'
+    NONE = 'none'
+
+    SOCKET = 'socket'
+    FAST_SOCKET = 'fast_socket'
+
