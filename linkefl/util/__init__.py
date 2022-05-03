e995ce9818de6df3749347a347aff1650f93c1d1
tanjuntao <iamtanjuntao@gmail.com>
2022-05-03 10:55:16 +0800

    move the factory functions into common package

--- linkefl/util/__init__.py
+++ linkefl/util/__init__.py
@@ -1,7 +1,5 @@
 from .utils import (
     sigmoid,
-    crypto_factory, partial_crypto_factory,
-    messenger_factory,
     save_params, load_params,
     load_data, save_data,
     load_model, save_model
