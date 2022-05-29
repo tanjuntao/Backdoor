9fd79d9ad5afbd329c91490cd5c0568b59728a72
tanjuntao <iamtanjuntao@gmail.com>
2022-05-29 13:43:59 +0800

    delete old transform.py

--- linkefl/feature/__init__.py
+++ linkefl/feature/__init__.py
@@ -1,2 +1,4 @@
 from .importance import feature_ranking
-from .transform import add_intercept, normalize, scale, parse_label
+from .transform import add_intercept, normalize, parse_label, scale
+from .transform import AddIntercept, Normalize, ParseLabel, Scale
+from .transform import Compose
