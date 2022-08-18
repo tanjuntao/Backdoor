547f977c23fe76856b2313b224b668271cefca8c
tanjuntao <iamtanjuntao@gmail.com>
2022-08-18 17:03:29 +0800

    fix the loop-import bug in feature importance module

--- linkefl/feature/__init__.py
+++ linkefl/feature/__init__.py
@@ -1,4 +1,4 @@
-from .importance import feature_ranking
+from .importance import cal_importance_ranking
 from .transform import add_intercept, normalize, parse_label, scale
 from .transform import AddIntercept, Normalize, ParseLabel, Scale
 from .transform import Compose
