2ced76bc871771dd507dca0705b0d0d6ca9debf3
tanjuntao <iamtanjuntao@gmail.com>
2022-09-01 18:14:46 +0800

    add verticalNN example

---
+++ examples/psi-nn/active.py
@@ -0,0 +1,95 @@
+from termcolor import colored
+import torch
+from torch import nn
+
+from linkefl.common.const import Const
+from linkefl.common.factory import crypto_factory
+from linkefl.crypto import RSACrypto
+from linkefl.dataio import TorchDataset
+from linkefl.feature import scale, add_intercept
+from linkefl.messenger import FastSocket
+from linkefl.psi.rsa import RSAPSIActive
+from linkefl.vfl.nn import ActiveBottomModel, IntersectionModel, TopModel
+from linkefl.vfl.nn import ActiveNeuralNetwork
+
+
+
+if __name__ == '__main__':
+    # 0. Set parameters
+    trainset_path = '/Users/tanjuntao/LinkeFL/linkefl/data/tabular/give_me_some_credit_active_train.csv'
+    testset_path = '/Users/tanjuntao/LinkeFL/linkefl/data/tabular/give_me_some_credit_active_test.csv'
+    passive_feat_frac = 0.5
+    feat_perm_option = Const.SEQUENCE
+    active_ip = 'localhost'
+    active_port = 20000
+    passive_ip = 'localhost'
+    passive_port = 30000
+    _epochs = 80
+    _batch_size = 64
+    _learning_rate = 0.01
+    _crypto_type = Const.PLAIN
+    _key_size = 1024
+    _loss_fn = nn.CrossEntropyLoss()
+    bottom_nodes = [5, 3, 3]
+    intersect_nodes = [3, 3, 3]
+    top_nodes = [3, 2]
+
+    # 1. Load dataset
+    active_trainset = TorchDataset(role=Const.ACTIVE_NAME, abs_path=trainset_path)
+    active_testset = TorchDataset(role=Const.ACTIVE_NAME, abs_path=testset_path)
+    print(colored('1. Finish loading dataset.', 'red'))
+
+    # # 2. Feature transformation
+    # active_trainset = scale(add_intercept(active_trainset))
+    # active_testset = scale(add_intercept(active_testset))
+    # print(colored('2. Finish transforming features', 'red'))
+
+    # 3. Run PSI
+    messenger = FastSocket(role=Const.ACTIVE_NAME,
+                           active_ip=active_ip,
+                           active_port=active_port,
+                           passive_ip=passive_ip,
+                           passive_port=passive_port)
+    psi_crypto = RSACrypto()
+    active_psi = RSAPSIActive(active_trainset.ids, messenger, psi_crypto)
+    common_ids = active_psi.run()
+    active_trainset.filter(common_ids)
+    print(colored('3. Finish psi protocol', 'red'))
+
+    # 4. VFL training
+    bottom_model = ActiveBottomModel(bottom_nodes)
+    intersect_model = IntersectionModel(intersect_nodes)
+    top_model = TopModel(top_nodes)
+    _models = [bottom_model, intersect_model, top_model]
+    _optimizers = [torch.optim.SGD(model.parameters(), lr=_learning_rate)
+                   for model in _models]
+    vfl_crypto = crypto_factory(crypto_type=_crypto_type,
+                                key_size=_key_size,
+                                num_enc_zeros=10000,
+                                gen_from_set=False)
+    active_vfl = ActiveNeuralNetwork(epochs=_epochs,
+                                     batch_size=_batch_size,
+                                     models=_models,
+                                     optimizers=_optimizers,
+                                     loss_fn=_loss_fn,
+                                     messenger=messenger,
+                                     crypto_type=_crypto_type,
+                                     saving_model=True)
+    active_vfl.train(active_trainset, active_testset)
+
+    # 5. inference
+    scores = active_vfl.validate(active_testset)
+    print(scores)
+
+    # 6. Close messenger, finish training
+    messenger.close()
+
+
+    # For online inference, you only need to substitute the model name
+    # scores = ActiveNeuralNetwork.online_inference(
+    #     active_testset,
+    #     messenger,
+    #     model_arch=_models,
+    #     model_name='20220901_174600-active_party-vertical_nn-120000_samples.model',
+    # )
+    # print(scores)
---
+++ linkefl/vfl/nn/__init__.py
@@ -0,0 +1,3 @@
+from .active import ActiveNeuralNetwork
+from .model import ActiveBottomModel, IntersectionModel, PassiveBottomModel, TopModel
+from .passive import PassiveNeuralNetwork
