2f78417fe7899b49b6a867400297feda03047dc4
tanjuntao <iamtanjuntao@gmail.com>
2022-09-01 12:34:15 +0800

    modify PyTorch modelio module

--- linkefl/modelio/torch_model.py
+++ linkefl/modelio/torch_model.py
@@ -13,32 +13,60 @@ class TorchModelIO:
             os.mkdir(path)
 
         if epoch is not None and optimizer is not None:
-            checkpoint = {
-                'model': model.state_dict(),
-                'epoch': epoch,
-                'optimizer': optimizer.state_dict()
-            }
+            if type(model) == list: # model is a list composed of many submodels
+                checkpoint = {
+                    'model': [sub_model.state_dict() for sub_model in model],
+                    'epoch': epoch,
+                    'optimizer': [sub_optim.state_dict() for sub_optim in optimizer]
+                }
+            else: # single model
+                checkpoint = {
+                    'model': model.state_dict(),
+                    'epoch': epoch,
+                    'optimizer': optimizer.state_dict()
+                }
         else:
-            checkpoint = {
-                'model': model.state_dict()
-            }
+            if type(model) == list: # model is a list composed of many submodels
+                checkpoint = {
+                    'model': [sub_model.state_dict() for sub_model in model]
+                }
+            else: # single model
+                checkpoint = {
+                    'model': model.state_dict()
+                }
 
         torch.save(checkpoint, os.path.join(path, name))
 
     @staticmethod
-    def load(path, name, model_arch=None):
+    def load(model_arch, path, name, optimizer_arch=None):
         if not os.path.exists(path):
             raise Exception(f"{path} not found.")
-        if model_arch is None:
-            raise ValueError('model architecture should be provided')
 
         checkpoint = torch.load(os.path.join(path, name))
-        model = model_arch.load_state_dict(checkpoint['model'])
-        if 'epoch' not in checkpoint and 'optimizer' not in checkpoint:
-            epoch = None
-            optimizer = None
-        else:
-            epoch = checkpoint['epoch']
-            optimizer = checkpoint['optimizer']
 
-        return model, epoch, optimizer
+        # One detail that should be noted when loading a PyTorch model is that:
+        # you should directly do net.load_state_dict(torch.load(PATH)),
+        # rather than net = net.load_state_dict(torch.load(PATH))
+        # ref: https://stackoverflow.com/a/59442773/8418540
+        if 'epoch' in checkpoint and 'optimizer' in checkpoint:
+            epoch = checkpoint['epoch']
+            if type(checkpoint['model']) == list:
+                for sub_model, sub_model_arch, sub_optim, sub_optim_arch in zip(
+                    checkpoint['model'], model_arch, checkpoint['optimizer'], optimizer_arch
+                ):
+                    sub_model_arch.load_state_dict(sub_model)
+                    sub_optim_arch.load_state_dict(sub_optim)
+            else:
+                model_arch.load_state_dict(checkpoint['model'])
+                optimizer_arch.load_state_dict(checkpoint['optimizer'])
+        else:
+            epoch = None
+            if type(checkpoint['model']) == list:
+                optimizer_arch = [None] * len(checkpoint['model'])
+                for sub_model, sub_model_arch in zip(checkpoint['model'], model_arch):
+                    sub_model_arch.load_state_dict(sub_model)
+            else:
+                optimizer_arch = None
+                model_arch.load_state_dict(checkpoint['model'])
+
+        return model_arch, epoch, optimizer_arch
