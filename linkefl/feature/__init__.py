11a06206eaf6d8a7035c2f50e7830b6c5fe4abac
tanjuntao <iamtanjuntao@gmail.com>
2022-09-16 00:17:34 +0800

    add OneHot to linkefl.feature subpackage

--- linkefl/feature/__init__.py
+++ linkefl/feature/__init__.py
@@ -1,4 +1,4 @@
 from .importance import cal_importance_ranking
-from .transform import add_intercept, normalize, parse_label, scale
-from .transform import AddIntercept, Normalize, ParseLabel, Scale
+from .transform import add_intercept, normalize, parse_label, scale, one_hot
+from .transform import AddIntercept, Normalize, OneHot, ParseLabel, Scale
 from .transform import Compose
--- linkefl/feature/transform/__init__.py
+++ linkefl/feature/transform/__init__.py
@@ -1,2 +1,2 @@
-from .functional import scale, normalize, add_intercept, parse_label
-from .transform import Scale, Normalize, AddIntercept, ParseLabel, Compose
+from .functional import scale, normalize, add_intercept, parse_label, one_hot
+from .transform import Scale, Normalize, AddIntercept, ParseLabel, Compose, OneHot
