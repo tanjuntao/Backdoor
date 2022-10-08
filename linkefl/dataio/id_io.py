89aa2d924c4b49bb5270fd6d8a4b57be39bf4b5a
tanjuntao <iamtanjuntao@gmail.com>
2022-10-08 15:15:21 +0800

    update gen_dummy_id function to remove duplicated items

--- linkefl/dataio/id_io.py
+++ linkefl/dataio/id_io.py
@@ -30,16 +30,25 @@ def get_ids(role, config, largest_val=pow(2, 512)):
     return ids
 
 
-def gen_dummy_ids(size, option=Const.SEQUENCE, largest=pow(2, 512)):
-    assert option in (Const.RANDOM, Const.SEQUENCE), \
-        'option can only take `random` and `sequence`, ' \
-        'but {} got instead'.format(option)
+def gen_dummy_ids(size, option=Const.SEQUENCE, largest=pow(2, 512), seed=None):
+    if option not in (Const.RANDOM, Const.SEQUENCE):
+        raise ValueError("option can only be taken from random and sequence, "
+                         "but got {} instead".format(option))
+    if size > largest:
+        raise ValueError("size should not be larger than largetst.")
 
     if option == Const.SEQUENCE:
-        ids = [i for i in range(size)]
+        ids = [i for i in range(1, size + 1)] # starting from 1
     else:
-        ids = [random.SystemRandom().randrange(1, largest) for _ in range(size)]
+        if seed is not None:
+            random.seed(seed)
+        ids = set()
+        while len(ids) != size:
+            ids.add(random.randrange(1, largest))
 
     return ids
 
 
+if __name__ == '__main__':
+    ids_ = gen_dummy_ids(10000000, option=Const.RANDOM, largest=1 << 30, seed=0)
+    print(len(ids_))
