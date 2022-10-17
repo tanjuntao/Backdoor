f51f82e298914167e7d8fe9053b0d0aed715d22e
mac <1329753495@qq.com>
2022-10-17 19:26:40 +0800

    temp file

--- linkefl/vfl/tree/data_functions.py
+++ linkefl/vfl/tree/data_functions.py
@@ -167,6 +167,78 @@ def get_bin_info(x_train, max_bin):
 
     return bin_index, bin_split
 
+def random_sampling(grad, hess, sample_rate):
+    """
+
+    Args:
+        grad:
+        hess:
+        sample_rate:
+
+    Returns:
+
+    """
+
+def goss_sampling(grad, hess, top_rate, other_rate):
+    """
+    Sample-level sampling method proposed in lightGBM.
+
+    Args:
+        grad: list[float]
+        hess: list[float]
+
+    Returns:
+        list, [select_grad, select_hess, select_idx.]
+    """
+    # if it is multi-classification case, we need to sum g
+    if len(grad.shape) > 1:
+        abs_g_sum_arr = np.abs(grad).sum(axis=1)
+    else:
+        abs_g_sum_arr = np.abs(grad)
+
+    # abs_g_list_arr = g_sum_arr
+    sorted_idx = np.argsort(-abs_g_sum_arr, kind='stable')  # stable sample result
+
+    sample_num = len(abs_g_sum_arr)
+    a_part_num = int(sample_num * top_rate)
+    b_part_num = int(sample_num * other_rate)
+
+    if a_part_num == 0 or b_part_num == 0:
+        raise ValueError('subsampled result is 0: top sample {}, other sample {}'.format(a_part_num, b_part_num))
+
+    # index of a part
+    a_sample_idx = sorted_idx[:a_part_num]
+
+    # index of b part
+    rest_sample_idx = sorted_idx[a_part_num:]
+    b_sample_idx = np.random.choice(rest_sample_idx, size=b_part_num, replace=False)
+
+    # small gradient sample weights
+    # amplify_weights = (1 - top_rate) / other_rate
+    amplify_weights = 1
+    grad[b_sample_idx] *= amplify_weights
+    hess[b_sample_idx] *= amplify_weights
+
+    # get selected sample
+    a_idx_set, b_idx_set = set(list(a_sample_idx)), set(list(b_sample_idx))
+    idx_set = a_idx_set.union(b_idx_set)
+    selected_idx = np.array(list(idx_set))
+
+    selected_g, selected_h = grad[selected_idx], hess[selected_idx]
+
+    return selected_g, selected_h, selected_idx
 
 def wrap_message(name, *, content):
     return {"name": name, "content": content}
+
+if __name__ == "__main__":
+    sample_num = 20
+    grad, hess = np.random.random(sample_num), np.random.random(sample_num)
+    selected_g, selected_h, selected_idx = goss_sampling(grad, hess, 0.25, 0.25)
+
+    g_enc, h_enc = [0 for _ in range(sample_num)], [0 for _ in range(sample_num)]
+    for i, idx in enumerate(selected_idx):
+        g_enc[idx] = selected_g[i]
+        h_enc[idx] = selected_h[i]
+
+    print(grad, hess)
