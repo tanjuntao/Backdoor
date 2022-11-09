dbf33c14a5f533e65d34f835de05e9a04be1ba89
apylers <apylers@gmail.com>
2022-11-09 15:42:50 +0800

    test(examples): add a multiple passive party pipeline example in tree

--- examples/psi-tree/passive2_pipeline.py
+++ examples/psi-tree/passive2_pipeline.py
@@ -1,8 +1,7 @@
 from linkefl.common.const import Const
-from linkefl.common.factory import logger_factory
+from linkefl.common.factory import logger_factory, messenger_factory
 from linkefl.dataio import NumpyDataset
 from linkefl.feature.transform import Compose, ParseLabel
-from linkefl.messenger import FastSocket
 from linkefl.pipeline import PipeLine
 from linkefl.psi import CM20PSIPassive
 from linkefl.vfl.tree import PassiveTreeParty
@@ -11,17 +10,17 @@ if __name__ == "__main__":
     # 0. Set parameters
 
     # dataloader
-    dataset_name = "credit"
-    passive_feat_frac = 0.5
-    feat_perm_option = Const.SEQUENCE
+    trainset_path = "census-passive2-train.csv"
+    testset_path = "census-passive2-test.csv"
 
     # messenger
     active_ip = "localhost"
-    active_port = 20000
+    active_port = 30000
     passive_ip = "localhost"
-    passive_port = 30000
+    passive_port = 30001
 
-    messenger = FastSocket(
+    messenger = messenger_factory(
+        messenger_type=Const.FAST_SOCKET,
         role=Const.PASSIVE_NAME,
         active_ip=active_ip,
         active_port=active_port,
@@ -32,39 +31,53 @@ if __name__ == "__main__":
     # logger
     logger = logger_factory(role=Const.PASSIVE_NAME)
 
+    # psi
+    log_height = 8
+    width = 632
+    hash_length = 10
+    h1_length = 32
+    bucket1 = 1 << 8
+    bucket2 = 1 << 8
+
     # transformer
 
     # model
     task = "binary"
     crypto_type = Const.FAST_PAILLIER
+    max_bin = 16
+    colsample_bytree = 1
 
     # 1. Load dataset
-    passive_trainset = NumpyDataset.buildin_dataset(
+    passive_trainset = NumpyDataset.from_csv(
         role=Const.PASSIVE_NAME,
-        dataset_name=dataset_name,
-        root="data",
-        train=True,
-        passive_feat_frac=passive_feat_frac,
-        feat_perm_option=feat_perm_option,
-        download=True,
+        abs_path=trainset_path,
+        dataset_type=Const.CLASSIFICATION,
     )
-    passive_testset = NumpyDataset.buildin_dataset(
+    passive_testset = NumpyDataset.from_csv(
         role=Const.PASSIVE_NAME,
-        dataset_name=dataset_name,
-        root="data",
-        train=False,
-        passive_feat_frac=passive_feat_frac,
-        feat_perm_option=feat_perm_option,
-        download=True,
+        abs_path=testset_path,
+        dataset_type=Const.CLASSIFICATION,
+        mappings=passive_trainset.mappings,
     )
 
     # 2. Build pipeline
-    psi = CM20PSIPassive(messenger, logger)
+    psi = CM20PSIPassive(
+        messenger,
+        logger,
+        # log_height=log_height,
+        # width=width,
+        # hash_length=hash_length,
+        # h1_length=h1_length,
+        # bucket1=bucket1,
+        # bucket2=bucket2,
+    )
     transforms = Compose([ParseLabel()])
     model = PassiveTreeParty(
         task=task,
         crypto_type=crypto_type,
         messenger=messenger,
+        max_bin=max_bin,
+        colsample_bytree=colsample_bytree,
     )
 
     pipeline = PipeLine([psi, transforms, model], role=Const.PASSIVE_NAME)
