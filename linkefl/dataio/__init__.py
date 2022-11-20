ac45911bbfff7e77b5afce39ad4d8f9142c06e65
tanjuntao <iamtanjuntao@gmail.com>
2022-11-20 10:32:57 +0800

    fix(dataio): remove deleted imports

--- linkefl/dataio/__init__.py
+++ linkefl/dataio/__init__.py
@@ -1,5 +1,4 @@
 from .base import BaseDataset
 from .id_io import gen_dummy_ids
 from .np_dataset import NumpyDataset
-from .tensor_io import get_tensor_dataset
 from .torch_dataset import TorchDataset
