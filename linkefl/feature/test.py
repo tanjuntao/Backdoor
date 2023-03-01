57312f3630e183a8b713b552793bda843c197eeb
huangju <1302189881@qq.com>
2023-03-01 16:50:32 +0800

    feat[woe]: Realize the calculation of woe and iv under encryption, and add corresponding usage examples.

--- linkefl/feature/test.py
+++ linkefl/feature/test.py
@@ -4,11 +4,13 @@ import pandas as pd
 from linkefl.common.const import Const
 from linkefl.common.factory import crypto_factory, messenger_factory
 from linkefl.dataio import NumpyDataset
-from linkefl.feature.chi_square_bin import ActiveChiBin
-from linkefl.feature.feature_evaluation import FeatureEvaluation
-from linkefl.feature.pearson import ActivePearsonVfl
 from linkefl.feature.transform import parse_label
+from linkefl.feature.feature_evaluation import FeatureEvaluation
 from linkefl.feature.woe import ActiveWoe
+from linkefl.feature.chi_square_bin import ActiveChiBin
+from linkefl.feature.pearson import ActivePearsonVfl
+from linkefl.dataio import TorchDataset
+
 
 if __name__ == "__main__":
     # 0. Set parameters
@@ -34,26 +36,40 @@ if __name__ == "__main__":
 
     # 1. Load datasets
     print("Loading dataset...")
-    active_trainset = NumpyDataset.buildin_dataset(
-        role=Const.ACTIVE_NAME,
+    # active_trainset = NumpyDataset.buildin_dataset(role=Const.ACTIVE_NAME,
+    #                                                dataset_name=dataset_name,
+    #                                                root='../vfl/data',
+    #                                                train=True,
+    #                                                download=True,
+    #                                                passive_feat_frac=passive_feat_frac,
+    #                                                feat_perm_option=feat_perm_option)
+    # active_testset = NumpyDataset.buildin_dataset(role=Const.ACTIVE_NAME,
+    #                                               dataset_name=dataset_name,
+    #                                               root='../vfl/data',
+    #                                               train=False,
+    #                                               download=True,
+    #                                               passive_feat_frac=passive_feat_frac,
+    #                                               feat_perm_option=feat_perm_option)
+    active_trainset = TorchDataset.buildin_dataset(
         dataset_name=dataset_name,
+        role=Const.ACTIVE_NAME,
         root="../vfl/data",
         train=True,
         download=True,
         passive_feat_frac=passive_feat_frac,
         feat_perm_option=feat_perm_option,
     )
-    active_testset = NumpyDataset.buildin_dataset(
-        role=Const.ACTIVE_NAME,
+    active_testset = TorchDataset.buildin_dataset(
         dataset_name=dataset_name,
+        role=Const.ACTIVE_NAME,
         root="../vfl/data",
         train=False,
         download=True,
         passive_feat_frac=passive_feat_frac,
         feat_perm_option=feat_perm_option,
     )
-    active_trainset = parse_label(active_trainset)
-    active_testset = parse_label(active_testset)
+    # active_trainset = parse_label(active_trainset)
+    # active_testset = parse_label(active_testset)
     print("Done")
     # 2. Initialize cryptosystem
     _crypto = crypto_factory(
@@ -62,60 +78,41 @@ if __name__ == "__main__":
 
     # 3. Initialize messenger
     _messenger = [
-        messenger_factory(
-            messenger_type=Const.FAST_SOCKET,
-            role=Const.ACTIVE_NAME,
-            active_ip=ac_ip,
-            active_port=ac_port,
-            passive_ip=pass_ip,
-            passive_port=pass_port,
-        )
-        for ac_ip, ac_port, pass_ip, pass_port in zip(
-            active_ip, active_port, passive_ip, passive_port
-        )
+        messenger_factory(messenger_type=Const.FAST_SOCKET,
+                          role=Const.ACTIVE_NAME,
+                          active_ip=ac_ip,
+                          active_port=ac_port,
+                          passive_ip=pass_ip,
+                          passive_port=pass_port,
+                          )
+        for ac_ip, ac_port, pass_ip, pass_port in
+        zip(active_ip, active_port, passive_ip, passive_port)
     ]
 
-    active_trainset = parse_label(active_trainset)
-    active_testset = parse_label(active_testset)
+    # active_trainset = parse_label(active_trainset)
+    # active_testset = parse_label(active_testset)
     print("Done")
 
-    importances, ranking = FeatureEvaluation.tree_importance(
-        active_trainset, task="binary", evaluation_way="xgboost"
-    )
-    print(importances, ranking)
+    # importances, ranking = FeatureEvaluation.tree_importance(active_trainset, task="binary",evaluation_way="xgboost")
+    # print(importances, ranking)
 
-    corr = FeatureEvaluation.collinearity_anay(
-        dateset=active_trainset, evaluation_way="pearson"
-    )
-    print(corr)
+    # corr = FeatureEvaluation.collinearity_anay(dateset=active_trainset, evaluation_way="pearson")
+    # print(corr)
 
-    feature_psi = FeatureEvaluation.calculate_psi(active_trainset, active_testset)
-    print(feature_psi)
+    # feature_psi = FeatureEvaluation.calculate_psi(active_trainset, active_testset)
+    # print(feature_psi)
 
-    # split, woe, iv = ActiveWoe(
-    #     dataset=active_trainset,
-    #     idxes=[2, 3],
-    #     messenger=_messenger
-    # ).cal_woe()
+    # 明文计算主动方本地的woe & iv值
+    # split, woe, iv = ActiveWoe(dataset=active_trainset, idxes=[2, 3], messenger=_messenger).cal_woe()
     # print(split, woe, iv)
+    # 密文计算被动方的woe & iv值
+    woe, iv = ActiveWoe(dataset=active_trainset, idxes=[2, 3], messenger=_messenger, cryptosystem=_crypto).cal_woe_encry()
+    print(woe, iv)
 
-    # chi_bin = ActiveChiBin(
-    #     dataset=active_trainset,
-    #     idxes=[2, 3],
-    #     messenger=_messenger,
-    #     max_group=200
-    # ).chi_bin()
+    # chi_bin = ActiveChiBin(dataset=active_trainset, idxes=[2, 3], messenger=_messenger, max_group=200).chi_bin()
     # print(chi_bin)
 
-    # ActivePearsonVfl(
-    #     dataset=active_trainset,
-    #     messenger=_messenger,
-    #     cryptosystem=_crypto
-    # ).pearson_vfl()
+    # ActivePearsonVfl(dataset=active_trainset, messenger=_messenger, cryptosystem=_crypto).pearson_vfl()
 
-    # pearson = ActivePearsonVfl(
-    #     dataset=active_trainset,
-    #     messenger=_messenger,
-    #     cryptosystem=_crypto
-    # ).pearson_single()
+    # pearson = ActivePearsonVfl(dataset=active_trainset, messenger=_messenger, cryptosystem=_crypto).pearson_single()
     # print(pearson)
--- linkefl/feature/woe.py
+++ linkefl/feature/woe.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
-import pandas as pd
 import torch
+import pandas as pd
 
 
 class Basewoe(ABC):
@@ -32,7 +32,7 @@ class Basewoe(ABC):
         bin_iv : dict
             The iv value of the woe-encoded feature.
             key(int) : The position of the column to be woe encoded.
-            value(list) : The iv values.
+            value(float) : The iv values.
         """
         self.dataset = dataset
         self.idxes = idxes
@@ -92,7 +92,7 @@ class Basewoe(ABC):
                 )
                 self.split[self.idxes[i]] = self.split[self.idxes[i]][1:-1]
                 woe = []
-                iv = []
+                iv = 0
                 for j in range(bin):
                     bin_sample = bin_feature == j
                     bin_num = np.count_nonzero(bin_sample)
@@ -108,7 +108,7 @@ class Basewoe(ABC):
                     bin_iv = (bin_positive_ratio - bin_negative_ratio) * bin_woe
                     bin_feature = np.where(bin_feature == j, bin_woe, bin_feature)
                     woe.append(bin_woe)
-                    iv.append(bin_iv)
+                    iv += bin_iv
                 self.bin_woe[self.idxes[i]] = woe
                 self.bin_iv[self.idxes[i]] = iv
                 features[:, self.idxes[i]] = bin_feature
@@ -129,7 +129,7 @@ class Basewoe(ABC):
 
 
 class ActiveWoe(Basewoe):
-    def __init__(self, dataset, idxes, messenger):
+    def __init__(self, dataset, idxes, messenger, cryptosystem=None):
         """Woe encodes the specified features and calculates their iv values for active.
 
         Parameters
@@ -138,19 +138,84 @@ class ActiveWoe(Basewoe):
         """
         super(ActiveWoe, self).__init__(dataset, idxes)
         self.messenger = messenger
+        self.cryptosystem = cryptosystem
 
     def cal_woe(self, modify=True):
         y = self.dataset.labels
-        for msger in self.messenger:
-            msger.send(y)
-        super()._cal_woe(y, "active", modify)
+        for msger in self.messenger: msger.send(y)
+        super()._cal_woe(y, 'active', modify)
 
         return self.split, self.bin_woe, self.bin_iv
 
+    def cal_woe_encry(self):
+        """
+        :returns
+        woe : list
+            The element : dic
+            representing the woe values corresponding to the features of a passive party performing woe calculation.
+                key(int) :  The feature column index for woe,
+                value(list) : The corresponding woe value.
+
+        iv : list
+            The element : dic
+            representing the iv values corresponding to the features of a passive party performing iv calculation.
+                key(int) :  The feature column index for iv,
+                value(float) : The corresponding iv value.
+        """
+        y = self.dataset.labels
+
+        enc_y = self.cryptosystem.encrypt_vector(y)
+        enc_1_y = self.cryptosystem.encrypt_vector(1-y)
+        for msger in self.messenger:
+            msger.send(enc_y)
+            msger.send(enc_1_y)
+
+        all_enc_sum = []
+        for msger in self.messenger:
+            all_enc_sum.append(msger.recv())
+        woe, iv = self._cal_woe_encry(all_enc_sum)
+        return woe, iv
+
+    def _cal_woe_encry(self, all_enc_sum):
+        y = self.dataset.labels
+        if isinstance(self.dataset.features, np.ndarray) or isinstance(self.dataset.features, torch.Tensor):
+            if isinstance(self.dataset.features, torch.Tensor):
+                y = y.numpy()
+            positive = np.count_nonzero(y == 1)
+            negative = np.count_nonzero(y == 0)
+            delta = 1e-07
+            woe = []
+            iv = []
+            for i in range(len(all_enc_sum)):
+                woe_i = {}
+                iv_i = {}
+                for idx, enc_sum_bin in all_enc_sum[i].items():
+                    sum_bin = self.cryptosystem.decrypt_vector(all_enc_sum[i][idx])
+                    woe_i_bin = []
+                    iv_i_bin = 0.0
+                    for k in range(len(sum_bin)):
+                        sum_bin[k] = self.cryptosystem.decrypt_vector(sum_bin[k])
+                        bin_positive_ratio = sum_bin[k][0] / positive
+                        bin_negative_ratio = sum_bin[k][1] / negative
+                        if bin_positive_ratio == 0 or bin_negative_ratio == 0:
+                            bin_woe = np.log((bin_positive_ratio + delta) / (bin_negative_ratio + delta))
+                        else:
+                            bin_woe = np.log(bin_positive_ratio / bin_negative_ratio)
+                        bin_iv = (bin_positive_ratio - bin_negative_ratio) * bin_woe
+                        woe_i_bin.append(bin_woe)
+                        iv_i_bin += bin_iv
+                    woe_i[idx] = woe_i_bin
+                    iv_i[idx] = iv_i_bin
+                woe.append(woe_i)
+                iv.append(iv_i)
+        else:
+            raise TypeError('dataset should be an instance of numpy.ndarray or torch.Tensor')
+        return woe, iv
+
 
 class PassiveWoe(Basewoe):
-    def __init__(self, dataset, idxes, messenger):
-        """Woe encodes the specified features and calculates their iv values for passive
+    def __init__(self, dataset, idxes, messenger, cryptosystem=None):
+        """Woe encodes the specified features and calculates their iv values for passive.
 
         Parameters
         ----------
@@ -158,6 +223,7 @@ class PassiveWoe(Basewoe):
         """
         super(PassiveWoe, self).__init__(dataset, idxes)
         self.messenger = messenger
+        self.cryptosystem = cryptosystem
 
     def cal_woe(self, modify=True):
         y = self.messenger.recv()
@@ -165,7 +231,40 @@ class PassiveWoe(Basewoe):
 
         return self.split, self.bin_woe, self.bin_iv
 
+    def cal_woe_encry(self):
+        enc_y = self.messenger.recv()
+        enc_1_y = self.messenger.recv()
+        enc_sum = self._cal_woe_encry(enc_y, enc_1_y)
+        self.messenger.send(enc_sum)
 
+    def _cal_woe_encry(self, enc_y, enc_1_y):
+        bin = 3
+        features = self.dataset.features
+        n_samples = self.dataset.n_samples
+        enc_sum = {}
+        if isinstance(features, np.ndarray) or isinstance(features, torch.Tensor):
+            if isinstance(features, torch.Tensor):
+                features = features.float().numpy()
+            for i in range(len(self.idxes)):
+                bin_feature, self.split[self.idxes[i]] = \
+                    pd.cut(features[:, self.idxes[i]], bin, labels=False, retbins=True)
+                self.split[self.idxes[i]] = self.split[self.idxes[i]][1: -1]
+                enc_bin_sum = []
+                for j in range(bin):
+                    bin_sample = (bin_feature == j)
+                    enc_bin_sum_y = 0
+                    enc_bin_sum_1_y = 0
+                    for k in range(n_samples):
+                        if bin_sample[k]:
+                            enc_bin_sum_y += enc_y[k]
+                            enc_bin_sum_1_y += enc_1_y[k]
+                    enc_bin_sum.append([enc_bin_sum_y, enc_bin_sum_1_y])
+                enc_sum[self.idxes[i]] = enc_bin_sum
+        else:
+            raise TypeError('dataset should be an instance of numpy.ndarray or torch.Tensor')
+        return enc_sum
+
+    
 class TestWoe(Basewoe):
     def __init__(self, dataset, idxes, messenger, split, bin_woe):
         super(TestWoe, self).__init__(dataset, idxes)
@@ -174,6 +273,7 @@ class TestWoe(Basewoe):
 
     def cal_woe(self):
         features = self.dataset.features
+        ids = np.array(self.dataset.ids)
         if isinstance(features, np.ndarray):
             features = features.astype(float)
             sam_num = self.dataset.n_samples
