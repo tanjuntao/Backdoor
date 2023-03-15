a6f419c6fef44449990b4924470d777d63b5665d
tanjuntao <iamtanjuntao@gmail.com>
2023-03-15 10:46:51 +0800

    Refactor linkefl.vfl.nn
    * Merge the functionalities from active.py into active_new.py, and then
      Delete the old active.py and rename active_new.py to active.py
    
    * The save operation is conducted on passive.py and passive_new.py
    
    * Delete the old model.py and move all models to linkefl.modelzoo
    
    * Add a new encode_pool for enc_layer when encoding numpy array
    
    * Re-organize the if __name__ == "__main__" clause

--- linkefl/modelzoo/__init__.py
+++ linkefl/modelzoo/__init__.py
@@ -1,7 +1,7 @@
 from .embedding import Embedding
 from .googlenet import GoogLeNet
 from .lenet import LeNet
-from .mlp import MLP
+from .mlp import MLP, CutLayer
 from .mobilenet import MobileNet
 from .resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
 from .tab_resnet import TabResNet
--- linkefl/vfl/nn/enc_layer.py
+++ linkefl/vfl/nn/enc_layer.py
@@ -1,9 +1,11 @@
 import multiprocessing
 import os
+from typing import Optional
 
 import numpy as np
 import torch
 
+from linkefl.base import BaseCryptoSystem, BaseMessenger, BasePartialCryptoSystem
 from linkefl.common.const import Const
 from linkefl.crypto.paillier import cipher_matmul, encode
 
@@ -11,47 +13,44 @@ from linkefl.crypto.paillier import cipher_matmul, encode
 class PassiveEncLayer:
     def __init__(
         self,
-        in_nodes,
-        out_nodes,
-        eta,
-        cryptosystem,
-        messenger,
-        num_workers=None,
-        random_state=None,
-        precision=0.001,
+        in_nodes: int,
+        out_nodes: int,
+        eta: float,
+        cryptosystem: BaseCryptoSystem,
+        messenger: BaseMessenger,
+        num_workers: int = 1,
+        random_state: Optional[int] = None,
+        encode_precision: float = 0.001,
     ):
         self.in_nodes = in_nodes
         self.out_nodes = out_nodes
         self.eta = eta
         self.cryptosystem = cryptosystem
         self.messenger = messenger
-        self.precision = precision
-
-        if num_workers is None:
-            num_workers = os.cpu_count()
         self.num_workers = num_workers
-        self.process_pool = multiprocessing.pool.Pool(self.num_workers)
-        self.thread_pool = multiprocessing.pool.ThreadPool(self.num_workers)
-        if cryptosystem.type == Const.PAILLIER:
-            # using thread_pool for Paillier encrypt_vector
-            self.enc_pool = self.thread_pool
+        if self.num_workers > 1:
+            if cryptosystem.type == Const.PAILLIER:
+                # using thread_pool for Paillier encrypt_vector
+                self.enc_pool = multiprocessing.pool.ThreadPool(self.num_workers)
+            else:
+                # using process_pool for FastPaillier encrypt_vector
+                self.enc_pool = multiprocessing.pool.Pool(self.num_workers)
         else:
-            # using process_pool for FastPaillier encrypt_vector
-            self.enc_pool = self.process_pool
+            self.enc_pool = None
         if random_state is not None:
             np.random.seed(random_state)
-        self.w_acc = np.random.rand(in_nodes, out_nodes).astype(np.float32)
+        self.encode_precision = encode_precision
+
+        self.w_acc: np.ndarray = np.random.rand(in_nodes, out_nodes).astype(np.float32)
 
     def fed_forward(self, a):
         enc_a = self.cryptosystem.encrypt_vector(
-            torch.flatten(a), True, self.num_workers, self.enc_pool
+            torch.flatten(a), pool=self.enc_pool
         )  # return a Python list
         enc_a = np.array(enc_a).reshape(a.shape)  # convert it to numpy array
         self.messenger.send(enc_a)
         enc_z_tilde = self.messenger.recv()  # numpy array, shape: (bs, output_nodes)
-        z_tilde = self.cryptosystem.decrypt_vector(
-            enc_z_tilde.flatten(), using_pool=True, thread_pool=self.thread_pool
-        )
+        z_tilde = self.cryptosystem.decrypt_vector(enc_z_tilde.flatten())
         z_tilde = [float(val) for val in z_tilde]  # TODO: strange mpfr dtype
         z_tilde = torch.tensor(z_tilde).reshape(enc_z_tilde.shape)
         # z_tilde = np.array(z_tilde).reshape(enc_z_tilde.shape)
@@ -62,9 +61,7 @@ class PassiveEncLayer:
         enc_w_tilde_grad = (
             self.messenger.recv()
         )  # numpy array, shape: (input_nodes, output_nodes)
-        w_tilde_grad = self.cryptosystem.decrypt_vector(
-            enc_w_tilde_grad.flatten(), using_pool=True, thread_pool=self.thread_pool
-        )
+        w_tilde_grad = self.cryptosystem.decrypt_vector(enc_w_tilde_grad.flatten())
         w_tilde_grad = [float(val) for val in w_tilde_grad]  # TODO: strange mpfr dtype
         w_tilde_grad = np.array(w_tilde_grad).reshape(enc_w_tilde_grad.shape)
 
@@ -72,7 +69,7 @@ class PassiveEncLayer:
         w_tilde_grad = w_tilde_grad - w_curr / self.eta
 
         enc_w_acc = self.cryptosystem.encrypt_vector(
-            self.w_acc.flatten(), True, self.num_workers, self.enc_pool
+            self.w_acc.flatten(), pool=self.enc_pool
         )
         enc_w_acc = np.array(enc_w_acc).reshape(self.w_acc.shape)
         self.messenger.send([w_tilde_grad, enc_w_acc])
@@ -81,40 +78,44 @@ class PassiveEncLayer:
         self.w_acc = self.w_acc + w_curr
 
     def close_pool(self):
-        self.process_pool.close()
-        self.thread_pool.close()
+        if self.enc_pool is not None:
+            self.enc_pool.close()
 
 
 class ActiveEncLayer:
     def __init__(
         self,
-        in_nodes,
-        out_nodes,
-        eta,
-        cryptosystem,
-        messenger,
-        num_workers=None,
-        random_state=None,
-        precision=0.001,
+        in_nodes: int,
+        out_nodes: int,
+        eta: float,
+        cryptosystem: BasePartialCryptoSystem,
+        messenger: BaseMessenger,
+        num_workers: int = 1,
+        random_state: Optional[int] = None,
+        encode_precision: float = 0.001,
     ):
         self.in_nodes = in_nodes
         self.out_nodes = out_nodes
         self.eta = eta
         self.cryptosystem = cryptosystem
         self.messenger = messenger
-        self.precision = precision
-
-        if num_workers is None:
-            num_workers = os.cpu_count()
         self.num_workers = num_workers
-        self.process_pool = multiprocessing.pool.Pool(self.num_workers)
-        self.thread_pool = multiprocessing.pool.ThreadPool(self.num_workers)
-        self.scheduler_pool = multiprocessing.pool.ThreadPool(self.num_workers)
-
+        if self.num_workers > 1:
+            self.thread_pool = multiprocessing.pool.ThreadPool(self.num_workers)
+            self.scheduler_pool = multiprocessing.pool.ThreadPool(self.num_workers)
+            self.encode_pool = multiprocessing.pool.Pool(self.num_workers)
+        else:
+            self.thread_pool = None
+            self.scheduler_pool = None
+            self.encode_pool = None
         if random_state is not None:
             np.random.seed(random_state)
-        self.w_tilde = np.random.rand(in_nodes, out_nodes).astype(np.float32)
-        self.curr_enc_a = None
+        self.encode_precision = encode_precision
+
+        self.w_tilde: np.ndarray = np.random.rand(in_nodes, out_nodes).astype(
+            np.float32
+        )
+        self.curr_enc_a: Optional[np.ndarray] = None
 
     def fed_forward(self):
         enc_a = self.messenger.recv()  # is a numpy array, shape: (bs, input_nodes)
@@ -122,7 +123,8 @@ class ActiveEncLayer:
         w_tilde_encode = encode(
             raw_data=self.w_tilde,
             raw_pub_key=self.cryptosystem.pub_key,
-            precision=self.precision,
+            precision=self.encode_precision,
+            pool=self.encode_pool,
         )
         enc_z_tilde = cipher_matmul(
             cipher_matrix=enc_a,
@@ -138,7 +140,8 @@ class ActiveEncLayer:
         grad_encode = encode(
             raw_data=grad,
             raw_pub_key=self.cryptosystem.pub_key,
-            precision=self.precision,
+            precision=self.encode_precision,
+            pool=self.encode_pool,
         )
         enc_w_tilde_grad = cipher_matmul(
             cipher_matrix=self.curr_enc_a.transpose(),
@@ -168,6 +171,7 @@ class ActiveEncLayer:
         return enc_a_grad
 
     def close_pool(self):
-        self.process_pool.close()
-        self.thread_pool.close()
-        self.scheduler_pool.close()
+        if self.thread_pool is not None:
+            self.thread_pool.close()
+            self.scheduler_pool.close()
+            self.encode_pool.close()
