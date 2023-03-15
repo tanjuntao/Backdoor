bd4335ef9bcdea9afb2646fb27e4f7683a17a0a6
tanjuntao <iamtanjuntao@gmail.com>
2023-03-15 11:11:03 +0800

    Rename psi_a_lot to psi-concurrent

--- examples/psi-concurrent/passives.py
+++ examples/psi-concurrent/passives.py
@@ -2,37 +2,35 @@ import multiprocessing
 
 from linkefl.common.const import Const
 from linkefl.common.factory import logger_factory
-from linkefl.crypto import RSA
 from linkefl.dataio import gen_dummy_ids
 from linkefl.messenger import FastSocket
-from linkefl.psi import RSAPSIActive
+from linkefl.psi import PassiveRSAPSI
 
 
 def task(port_pair):
     active_port, passive_port = port_pair
     _ids = gen_dummy_ids(size=10000, option=Const.SEQUENCE)
 
-    _messenger1 = FastSocket(
-        role=Const.ACTIVE_NAME,
+    _messenger = FastSocket(
+        role=Const.PASSIVE_NAME,
         active_ip="127.0.0.1",
         active_port=active_port,
         passive_ip="127.0.0.1",
         passive_port=passive_port,
     )
-
-    _messenger = [_messenger1]
     _logger = logger_factory(role=Const.ACTIVE_NAME)
-    _crypto = RSA()
-
-    active_party = RSAPSIActive(_messenger, _crypto, _logger, num_workers=1, obfuscated_rate=0.5)
-    intersections_ = active_party.run(_ids)
+    passive_party = PassiveRSAPSI(
+        messenger=_messenger,
+        logger=_logger,
+        num_workers=1,
+    )
+    intersections_ = passive_party.run(_ids)
     print(len(intersections_))
 
-    for msger_ in _messenger:
-        msger_.close()
+    _messenger.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     n = 10
     active_ports = [10000 + i for i in range(n)]
     passive_ports = [20000 + i for i in range(n)]
@@ -40,5 +38,3 @@ if __name__ == '__main__':
         pool.map(task, zip(active_ports, passive_ports))
         pool.close()
         pool.join()
-
-
