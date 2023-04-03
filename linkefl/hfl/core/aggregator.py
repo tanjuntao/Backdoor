2217ac3297a959e774b61dd9e1bc12a3818640db
JunhaoWang <1594260677@qq.com>
2023-04-03 16:54:37 +0800

    linreg gradient cut

--- linkefl/hfl/core/aggregator.py
+++ linkefl/hfl/core/aggregator.py
@@ -114,7 +114,7 @@ class Aggregator_client:
                     break
                 epoch_loss += loss.item()
                 loss.backward()
-                torch.nn.utils.clip_grad_norm(model.parameters(), 1000)
+                torch.nn.utils.clip_grad_norm(model.parameters(), 500)
                 optimizer.step()
         return model.state_dict(), epoch_loss
 
