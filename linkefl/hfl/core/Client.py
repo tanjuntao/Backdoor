e0872f6221b4165cc520c3253bc1d826ae5a6e01
JunhaoWang <1594260677@qq.com>
2023-04-03 15:27:26 +0800

    inference model_dir

--- linkefl/hfl/core/Client.py
+++ linkefl/hfl/core/Client.py
@@ -125,6 +125,8 @@ class Client(BaseModelComponent):
             raise Exception("Invalid aggregation rule")
 
     def fit(self, trainset, validset, role="client"):
+        print("trainset:",trainset)
+        print("validaset:",validset)
         train_set = self._init_dataloader(trainset)
 
         optimizer = self.optimizer
