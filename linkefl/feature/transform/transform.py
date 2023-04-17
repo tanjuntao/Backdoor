Fix id precision error caused by add_intercept
* In add_intercept(), the original np_dataset is concatenated with
  Another intercept column, this will cause the loss of float precision
  Of the original np_dataset when id column contains very big integers
  Increase the precision of the result np array of np.concate() will
  Solve this problem, but also, this will consume more memory

linkefl/feature/transform/transform.py
@@ -104,7 +104,12 @@ class AddIntercept(BaseTransformComponent):
         if isinstance(dataset, np.ndarray):
             if has_label:
                 n_samples = dataset.shape[0]
-                dataset = np.c_[dataset, np.ones(n_samples)]
+                # dataset = np.c_[dataset, np.ones(n_samples)]
+                dataset = np.concatenate(
+                    (dataset, np.ones(n_samples)[:, np.newaxis]),
+                    axis=1,
+                    dtype=np.float128,  # important: preserve precision of id column
+                )
             else:
                 pass  # no need to append an intercept column for passive party dataset
         elif isinstance(dataset, torch.Tensor):