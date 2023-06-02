3b468a4a059e3142cb29d2369d267692cc720b42
HaoRan Cheng <1329753495@qq.com>
2023-06-02 19:26:36 +0800

    feat(tree/test): add test file.

---
+++ linkefl/vfl/tree/test/multi/active.py
@@ -0,0 +1,158 @@
+from linkefl.common.const import Const
+from linkefl.common.factory import (
+    crypto_factory,
+    logger_factory,
+    messenger_factory,
+    messenger_factory_disconnection,
+)
+from linkefl.dataio import NumpyDataset
+from linkefl.feature.transform import parse_label
+from linkefl.vfl.tree.active import ActiveTreeParty
+import numpy as np
+import pandas as pd
+class Test:
+    @staticmethod
+    def get_dataset(dataset_name, passive_feat_frac, feat_perm_option):
+        print("Loading dataset...")
+        dataset = NumpyDataset.from_csv(
+            role=Const.ACTIVE_NAME,
+            abs_path="../../../data/tabular/tabmnist-active000.csv",
+            # abs_path="/Users/chr/Code/LinkeFL/linkefl/vfl/data/tabular/tabmnist-active000.csv",
+            dataset_type=Const.CLASSIFICATION
+        )
+        trainset, testset = NumpyDataset.train_test_split(
+            dataset=dataset, test_size=0.2, option="sequence", seed=0
+        )
+        print("Done")
+
+        return trainset, testset
+
+    @staticmethod
+    def get_crypto_system(crypto_type, key_size):
+        crypto_system = crypto_factory(
+            crypto_type=crypto_type,
+            key_size=key_size,
+            num_enc_zeros=2000,
+            gen_from_set=False,
+        )
+        return crypto_system
+    @staticmethod
+    def get_messengers(active_ips, active_ports, passive_ips, passive_ports, drop_protection):
+        if not drop_protection:
+            messengers = [
+                messenger_factory(
+                    messenger_type=Const.FAST_SOCKET,
+                    role=Const.ACTIVE_NAME,
+                    active_ip=active_ip,
+                    active_port=active_port,
+                    passive_ip=passive_ip,
+                    passive_port=passive_port,
+                )
+                for active_ip, active_port, passive_ip, passive_port in zip(
+                    active_ips, active_ports, passive_ips, passive_ports
+                )
+            ]
+        else:
+            messengers = [
+                messenger_factory_disconnection(
+                    messenger_type=Const.FAST_SOCKET_V1,
+                    role=Const.ACTIVE_NAME,
+                    model_type="Tree",  # used as tag to verify data
+                    active_ip=active_ip,
+                    active_port=active_port,
+                    passive_ip=passive_ip,
+                    passive_port=passive_port,
+                )
+                for active_ip, active_port, passive_ip, passive_port in zip(
+                    active_ips, active_ports, passive_ips, passive_ports
+                )
+            ]
+        return messengers
+
+if __name__ == "__main__":
+    # 0. Set parameters
+    #  binary: cancer, digits, epsilon, census, credit, default_credit, criteo
+    #  regression: diabetes
+    dataset_name = "tabmnist"
+    passive_feat_frac = 0.5
+    feat_perm_option = Const.SEQUENCE
+
+    n_trees = 5
+    task = "multi"
+    n_labels = 10
+    key_size = 1024
+
+    crypto_type = Const.FAST_PAILLIER           # Const.PLAIN, Const.FAST_PAILLIER
+    compress = True                    # False, True
+    training_mode = "lightgbm"          # "lightgbm", "xgboost"
+    sampling_method = "uniform"         # "uniform", "goss"
+    max_depth = 4
+    max_num_leaves = 8
+    subsample = 1
+    top_rate = 0.3
+    other_rate = 0.7
+    colsample_bytree = 1
+    saving_model = True
+    model_dir = "../../models/test_multi"
+    model_name = "active.model"
+
+
+    active_ips = ["localhost"]
+    active_ports = [21001]
+    passive_ips = ["localhost"]
+    passive_ports = [20001]
+    drop_protection = False
+    reconnect_ports = [30003]
+
+    active_trainset, active_testset = Test.get_dataset(dataset_name, passive_feat_frac, feat_perm_option)
+    crypto_system = Test.get_crypto_system(crypto_type, key_size)
+    messengers = Test.get_messengers(active_ips, active_ports, passive_ips, passive_ports, drop_protection)
+
+    # 4. Initialize active tree party and start training
+    logger = logger_factory(role=Const.ACTIVE_NAME)
+    active_party = ActiveTreeParty(
+        n_trees=n_trees,
+        task=task,
+        n_labels=n_labels,
+        crypto_type=crypto_type,
+        crypto_system=crypto_system,
+        messengers=messengers,
+        logger=logger,
+        compress=compress,
+        training_mode=training_mode,
+        sampling_method=sampling_method,
+        max_depth=max_depth,
+        max_num_leaves=max_num_leaves,
+        subsample=subsample,
+        top_rate=top_rate,
+        other_rate=other_rate,
+        colsample_bytree=colsample_bytree,
+        n_processes=6,
+        drop_protection=drop_protection,
+        reconnect_ports=reconnect_ports,
+        saving_model=True,
+        model_dir=model_dir,
+        model_name=model_name,
+    )
+
+    active_party.train(active_trainset, active_testset)
+
+    # preds = ActiveTreeParty.online_inference(
+    #     active_testset,
+    #     messengers,
+    #     logger,
+    #     model_dir=model_dir,
+    #     model_name=model_name,
+    # )
+    # print(preds)
+
+    # feature_importance_info = pd.DataFrame(
+    #     active_party.feature_importances_(importance_type='cover')
+    # )
+    # print(feature_importance_info)
+
+    # active_party.print_model_structure(tree_structure="VERTICAL")
+
+    # 5. Close messengers, finish training
+    for messenger in messengers:
+        messenger.close()
---
+++ linkefl/vfl/tree/test/regression/passive.py
@@ -0,0 +1,110 @@
+import pandas as pd
+
+from linkefl.common.const import Const
+from linkefl.common.factory import (
+    logger_factory,
+    messenger_factory,
+    messenger_factory_disconnection,
+)
+from linkefl.dataio import NumpyDataset
+from linkefl.vfl.tree.passive import PassiveTreeParty
+class Test:
+    @staticmethod
+    def get_dataset(dataset_name, passive_feat_frac, feat_perm_option):
+        print("Loading dataset...")
+        passive_trainset = NumpyDataset.buildin_dataset(
+            role=Const.PASSIVE_NAME,
+            dataset_name=dataset_name,
+            root="../../../data",
+            train=True,
+            download=True,
+            passive_feat_frac=passive_feat_frac,
+            feat_perm_option=feat_perm_option,
+        )
+        passive_testset = NumpyDataset.buildin_dataset(
+            role=Const.PASSIVE_NAME,
+            dataset_name=dataset_name,
+            root="../../../data",
+            train=False,
+            download=True,
+            passive_feat_frac=passive_feat_frac,
+            feat_perm_option=feat_perm_option,
+        )
+        passive_trainset = NumpyDataset.feature_split(passive_trainset, n_splits=1)[0]
+        passive_testset = NumpyDataset.feature_split(passive_testset, n_splits=1)[0]
+        print("Done")
+
+        return passive_trainset, passive_testset
+    @staticmethod
+    def get_messengers(active_ip, active_port, passive_ip, passive_port, drop_protection):
+        if not drop_protection:
+            messenger = messenger_factory(
+                messenger_type=Const.FAST_SOCKET,
+                role=Const.PASSIVE_NAME,
+                active_ip=active_ip,
+                active_port=active_port,
+                passive_ip=passive_ip,
+                passive_port=passive_port,
+            )
+        else:
+            messenger = messenger_factory_disconnection(
+                messenger_type=Const.FAST_SOCKET_V1,
+                role=Const.PASSIVE_NAME,
+                model_type="Tree",
+                active_ip=active_ip,
+                active_port=active_port,
+                passive_ip=passive_ip,
+                passive_port=passive_port,
+            )
+        return messenger
+
+if __name__ == "__main__":
+    # 0. Set parameters
+    #  binary: cancer, digits, epsilon, census, credit, default_credit, criteo
+    #  regression: diabetes
+    dataset_name = "diabetes"
+    passive_feat_frac = 0.9
+    feat_perm_option = Const.SEQUENCE
+
+    task = "regression"
+    crypto_type = Const.FAST_PAILLIER   # Const.PLAIN, Const.FAST_PAILLIER
+    colsample_bytree = 1
+    saving_model = True
+    model_dir = "../../models/test_regression"
+    model_name = "passive.model"
+
+    active_ip = "localhost"
+    active_port = 21001
+    passive_ip = "localhost"
+    passive_port = 20001
+    drop_protection = False
+    reconnect_ports = 30003
+
+    passive_trainset, passive_testset = Test.get_dataset(dataset_name, passive_feat_frac, feat_perm_option)
+    messenger = Test.get_messengers(active_ip, active_port, passive_ip, passive_port, drop_protection)
+
+    # 4. Initialize active tree party and start training
+    logger = logger_factory(role=Const.PASSIVE_NAME)
+    passive_party = PassiveTreeParty(
+        task=task,
+        crypto_type=crypto_type,
+        messenger=messenger,
+        logger=logger,
+        saving_model=True,
+        model_dir=model_dir,
+        model_name=model_name,
+    )
+
+    passive_party.train(passive_trainset, passive_testset)
+
+    # passive_party.online_inference(passive_testset, messenger, logger,
+    #                                model_dir=model_dir,
+    #                                model_name=model_name,)
+
+    feature_importance_info = pd.DataFrame(
+        passive_party.feature_importances_(importance_type="cover")
+    )
+    print(feature_importance_info)
+
+    # 4. Close messenger, finish training
+    messenger.close()
