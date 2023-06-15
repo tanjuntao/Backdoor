33d3f514d848728e24e0fe31d54c8b1db760ff7c
apylers <apylers@gmail.com>
2023-06-15 15:00:34 +0800

    fix(vfl/tree): fix __init__

--- linkefl/vfl/tree/__init__.py
+++ linkefl/vfl/tree/__init__.py
@@ -1,6 +1,2 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-# @Project : LinkeFL 
-# @File    : __init__.py.py
-# @Author  : HaoRan Cheng
-# @Date    : 2023/5/9 16:56
+from .active import ActiveTreeParty
+from .passive import PassiveTreeParty
