3b6534e432189cd8f6c39955a4261bac6b796b8c
tanjuntao <iamtanjuntao@gmail.com>
2023-11-29 20:51:51 +0800

    [modelzoo]Use adaptive pooling layer to adapt for different image size

--- linkefl/modelzoo/resnet.py
+++ linkefl/modelzoo/resnet.py
@@ -106,7 +106,8 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, out.size()[2:])  # the resulution now is 4x4
+        # out = F.avg_pool2d(out, out.size()[2:])  # the resulution now is 4x4
+        out = F.adaptive_avg_pool2d(out, (1, 1))
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
