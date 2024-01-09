62953cb36b6de6cbb2e4234aa41315e3d4245fd6
tanjuntao <iamtanjuntao@gmail.com>
2024-01-09 17:07:23 +0800

    Autoflake: ignore init module imports

---
+++ linkefl/modelzoo/__init__.py
@@ -0,0 +1,11 @@
+from .cae import AutoEncoder
+from .embedding import Embedding
+from .googlenet import GoogLeNet
+from .lenet import LeNet
+from .mlp import MLP, CutLayer
+from .mobilenet import MobileNet
+from .passport import ConvPassportBlock, LinearPassportBlock
+from .resnet import FedPassResNet18, ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
+from .tab_resnet import TabResNet
+from .vgg import VGG11, VGG13, VGG16, VGG19
+from .vib import DeepVIB
