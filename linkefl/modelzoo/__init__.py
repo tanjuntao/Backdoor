30b19c1f910ab140e87f52c8bcd51e1017ac9914
tanjuntao <iamtanjuntao@gmail.com>
2024-01-09 16:24:08 +0800

    Modelzoo: import new models to modelzoo package

--- linkefl/modelzoo/__init__.py
+++
@@ -1,8 +0,0 @@
-from .embedding import Embedding
-from .googlenet import GoogLeNet
-from .lenet import LeNet
-from .mlp import MLP, CutLayer
-from .mobilenet import MobileNet
-from .resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
-from .tab_resnet import TabResNet
-from .vgg import VGG11, VGG13, VGG16, VGG19
