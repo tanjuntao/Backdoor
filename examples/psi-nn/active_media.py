3019091b8565c39b0d0ea3cd61df31ecc3299a57
tanjuntao <iamtanjuntao@gmail.com>
2024-01-19 15:24:05 +0800

    Support tab_mnist and tab_fashion_mnist

--- examples/psi-nn/active_media.py
+++ examples/psi-nn/active_media.py
@@ -6,9 +6,10 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 
 from linkefl.common.const import Const
 from linkefl.common.factory import logger_factory
-from linkefl.dataio import MediaDataset
+from linkefl.dataio import MediaDataset, TorchDataset
 from linkefl.messenger import EasySocketServer
 from linkefl.modelzoo import *  # noqa: F405
+from linkefl.util import num_input_nodes
 from linkefl.vfl.nn import ActiveNeuralNetwork
 
 args = get_args()
@@ -60,24 +61,64 @@ if __name__ == "__main__":
         _cut_nodes = [10, 10]
         _n_classes = 10
         _top_nodes = [10, _n_classes]
+    elif args.dataset in ("tab_mnist", "tab_fashion_mnist"):
+        _batch_size = 128
+        _dataset_dir = f"{data_prefix}/data"
+        topk = 1
+        _cut_nodes = [128, 64]
+        _n_classes = 10
+        _top_nodes = [64, _n_classes]
+    elif args.dataset == "criteo":
+        _batch_size = 256
+        _dataset_dir = f"{data_prefix}/data"
+        topk = 1
+        _cut_nodes = [10, 10]
+        _n_classes = 2
+        _top_nodes = [10, _n_classes]
     else:
         raise ValueError(f"{args.dataset} is not valid dataset.")
 
     # Load dataset
-    active_trainset = MediaDataset(
-        role=Const.ACTIVE_NAME,
-        dataset_name=args.dataset,
-        root=_dataset_dir,
-        train=True,
-        download=True,
-    )
-    active_testset = MediaDataset(
-        role=Const.ACTIVE_NAME,
-        dataset_name=args.dataset,
-        root=_dataset_dir,
-        train=False,
-        download=True,
-    )
+    if args.model in ("resnet18", "vgg13", "lenet"):
+        active_trainset = MediaDataset(
+            role=Const.ACTIVE_NAME,
+            dataset_name=args.dataset,
+            root=_dataset_dir,
+            train=True,
+            download=True,
+        )
+        active_testset = MediaDataset(
+            role=Const.ACTIVE_NAME,
+            dataset_name=args.dataset,
+            root=_dataset_dir,
+            train=False,
+            download=True,
+        )
+    elif args.model == "mlp":
+        _passive_feat_frac = 0.5
+        _feat_perm_option = Const.SEQUENCE
+        active_trainset = TorchDataset.buildin_dataset(
+            dataset_name=args.dataset,
+            role=Const.ACTIVE_NAME,
+            root=_dataset_dir,
+            train=True,
+            download=True,
+            passive_feat_frac=_passive_feat_frac,
+            feat_perm_option=_feat_perm_option,
+            seed=_random_state,
+        )
+        active_testset = TorchDataset.buildin_dataset(
+            dataset_name=args.dataset,
+            role=Const.ACTIVE_NAME,
+            root=_dataset_dir,
+            train=False,
+            download=True,
+            passive_feat_frac=_passive_feat_frac,
+            feat_perm_option=_feat_perm_option,
+            seed=_random_state,
+        )
+    else:
+        raise ValueError(f"{args.model} is not an valid model type.")
     print(colored("1. Finish loading dataset.", "red"))
 
     # Init models
@@ -91,6 +132,22 @@ if __name__ == "__main__":
         if args.dataset == "svhn":
             in_channel = 3
         bottom_model = LeNet(in_channel=in_channel, num_classes=_n_classes).to(_device)
+    elif args.model == "mlp":
+        input_nodes = num_input_nodes(
+            dataset_name=args.dataset,
+            role=Const.ACTIVE_NAME,
+            passive_feat_frac=_passive_feat_frac,
+        )
+        if args.dataset in ("tab_mnist", "tab_fashion_mnist"):
+            bottom_nodes = [input_nodes, 256, 128, 128]
+        else:
+            bottom_nodes = [input_nodes, 15, 10]
+        bottom_model = MLP(
+            bottom_nodes,
+            activate_input=False,
+            activate_output=True,
+            random_state=_random_state,
+        ).to(_device)
     else:
         raise ValueError(f"{args.model} is not an valid model type.")
     cut_layer = CutLayer(*_cut_nodes, random_state=_random_state).to(_device)
--- examples/psi-nn/passive_media.py
+++ examples/psi-nn/passive_media.py
@@ -6,9 +6,10 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 from linkefl.common.const import Const
 from linkefl.common.factory import logger_factory
 from linkefl.crypto import Plain
-from linkefl.dataio import MediaDataset
+from linkefl.dataio import MediaDataset, TorchDataset
 from linkefl.messenger import EasySocket
 from linkefl.modelzoo import *  # noqa
+from linkefl.util import num_input_nodes
 from linkefl.vfl.nn import PassiveNeuralNetwork
 
 args = get_args()
@@ -51,24 +52,63 @@ if __name__ == "__main__":
         topk = 1
         _cut_nodes = [10, 10]
         num_classes = 10
+    elif args.dataset in ("tab_mnist", "tab_fashion_mnist"):
+        _batch_size = 128
+        _dataset_dir = f"{data_prefix}/data"
+        topk = 1
+        _cut_nodes = [128, 64]
+        num_classes = 10
+    elif args.dataset == "criteo":
+        _batch_size = 256
+        _dataset_dir = f"{data_prefix}/data"
+        topk = 1
+        _cut_nodes = [10, 10]
+        num_classes = 2
     else:
         raise ValueError(f"{args.dataset} is not valid dataset.")
 
     # Load dataset
-    passive_trainset = MediaDataset(
-        role=Const.PASSIVE_NAME,
-        dataset_name=args.dataset,
-        root=_dataset_dir,
-        train=True,
-        download=True,
-    )
-    passive_testset = MediaDataset(
-        role=Const.PASSIVE_NAME,
-        dataset_name=args.dataset,
-        root=_dataset_dir,
-        train=False,
-        download=True,
-    )
+    if args.model in ("resnet18", "vgg13", "lenet"):
+        passive_trainset = MediaDataset(
+            role=Const.PASSIVE_NAME,
+            dataset_name=args.dataset,
+            root=_dataset_dir,
+            train=True,
+            download=True,
+        )
+        passive_testset = MediaDataset(
+            role=Const.PASSIVE_NAME,
+            dataset_name=args.dataset,
+            root=_dataset_dir,
+            train=False,
+            download=True,
+        )
+    elif args.model == "mlp":
+        _passive_feat_frac = 0.5
+        _feat_perm_option = Const.SEQUENCE
+        passive_trainset = TorchDataset.buildin_dataset(
+            dataset_name=args.dataset,
+            role=Const.PASSIVE_NAME,
+            root=_dataset_dir,
+            train=True,
+            download=True,
+            passive_feat_frac=_passive_feat_frac,
+            feat_perm_option=_feat_perm_option,
+            seed=_random_state,
+        )
+        passive_testset = TorchDataset.buildin_dataset(
+            dataset_name=args.dataset,
+            role=Const.PASSIVE_NAME,
+            root=_dataset_dir,
+            train=False,
+            download=True,
+            passive_feat_frac=_passive_feat_frac,
+            feat_perm_option=_feat_perm_option,
+            seed=_random_state,
+        )
+    else:
+        raise ValueError(f"{args.model} is not an valid model type.")
+
     print(colored("1. Finish loading dataset.", "red"))
 
     # Init models
@@ -82,6 +122,22 @@ if __name__ == "__main__":
         if args.dataset == "svhn":
             in_channel = 3
         bottom_model = LeNet(in_channel=in_channel, num_classes=num_classes).to(_device)
+    elif args.model == "mlp":
+        input_nodes = num_input_nodes(
+            dataset_name=args.dataset,
+            role=Const.PASSIVE_NAME,
+            passive_feat_frac=_passive_feat_frac,
+        )
+        if args.dataset in ("tab_mnist", "tab_fashion_mnist"):
+            bottom_nodes = [input_nodes, 256, 128, 128]
+        else:
+            bottom_nodes = [input_nodes, 15, 10]
+        bottom_model = MLP(
+            bottom_nodes,
+            activate_input=False,
+            activate_output=True,
+            random_state=_random_state,
+        ).to(_device)
     else:
         raise ValueError(f"{args.model} is not an valid model type.")
     cut_layer = CutLayer(*_cut_nodes, random_state=_random_state).to(_device)
