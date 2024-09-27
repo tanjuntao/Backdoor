e0704d99bd070aa3742c9ee3e2f62cb501b9ab9f
tanjuntao <iamtanjuntao@gmail.com>
2024-09-27 09:36:43 +0800

    Update args parser

--- examples/psi-nn/args_parser.py
+++ examples/psi-nn/args_parser.py
@@ -10,40 +10,32 @@ def get_args():
     parser.add_argument(
         "--per_class", default=4, type=int, help="labeled samples per class"
     )
-    parser.add_argument("--layer", default="", type=str, help="number of mask layer")
-    parser.add_argument("--attempt", default=0, type=int, help="number of trying")
-    parser.add_argument("--defense", default="", type=str, help="defense method")
-    parser.add_argument("--scratch", action="store_true")
     parser.add_argument(
-        "--noise_scale", default=1e-5, type=float, help="ng noise scale"
-    )
-    parser.add_argument(
-        "--preserved_ratio", default=0.1, type=float, help="cg preserved ratio"
-    )
-    parser.add_argument("--bins_num", default=100, type=int, help="dg bins num")
-    parser.add_argument("--eps", default=1.0, type=float, help="labeldp epsilon")
-    parser.add_argument(
-        "--sigma2", default=1.0, type=float, help="fedpass secret key scale square"
-    )
-    parser.add_argument(
-        "--mid_weight", default=1.0, type=float, help="weight of vib loss in mid"
-    )
-    parser.add_argument("--dcor_weight", default=1.0, type=float, help="dcor weight")
-    parser.add_argument(
-        "--privacy_budget", default=0.5, type=float, help="privacy budget in vmask"
-    )
-    parser.add_argument(
-        "--shadow_per_class",
-        default=1000,
+        "--topk_confident",
+        default=50,
         type=int,
-        help="shadow number of samples per class",
+        help="number of topk confident samples",
     )
     parser.add_argument(
-        "--selection", default="", type=str, help="layer selection strategy"
+        "--poison_epochs",
+        default="",
+        type=str,
+        help="at which epochs to poison model training",
+    )
+
+    parser.add_argument("--scratch", action="store_true")
+    parser.add_argument("--attempt", default=0, type=int, help="number of trying")
+    parser.add_argument("--target", default=0, type=int, help="target class")
+    parser.add_argument(
+        "--weight_scale", default=1, type=int, help="weight scaling factor"
     )
     parser.add_argument(
         "--world_size", default=2, type=int, help="number of total participants"
     )
+    parser.add_argument(
+        "--alpha", default=1.0, type=float, help="trigger vector scaling factor"
+    )
+    parser.add_argument("--agg", type=str, default="add", help="aggregation function")
     parser.add_argument("--rank", default=0, type=int, help="party rank")
     args = parser.parse_args()
     return args
@@ -52,35 +44,20 @@ def get_args():
 def get_model_dir():
     args = get_args()
     model_dir = f"/home/1002tjt/models/{args.dataset}_{args.model}"
-    if args.defense == "ng":
-        model_dir += f"_ng_{args.noise_scale}"
-    elif args.defense == "cg":
-        model_dir += f"_cg_{args.preserved_ratio}"
-    elif args.defense == "dg":
-        model_dir += f"_dg_{args.bins_num}"
-    elif args.defense == "fedpass":
-        model_dir += f"_fedpass_{args.sigma2}"
-    elif args.defense == "labeldp":
-        model_dir += f"_labeldp_{args.eps}"
-    elif args.defense == "dcor":
-        model_dir += f"_dcor_{args.dcor_weight}"
-    elif args.defense == "mid":
-        model_dir += f"_mid_{args.mid_weight}"
-    elif args.defense == "vmask":
-        model_dir += f"_vmask_{args.privacy_budget}_{args.shadow_per_class}"
-        if args.selection:
-            model_dir += f"_{args.selection}"
-    elif args.defense == "":
-        if args.attempt != 0:
-            model_dir += f"_attempt_{args.attempt}"
-    else:
-        raise ValueError(f"{args.defense} is not supported.")
+    if args.weight_scale != 1:
+        model_dir += f"_scale{args.weight_scale}"
+    if args.agg != "add":
+        model_dir += f"_{args.agg}"
+    if args.poison_epochs != "":
+        model_dir += f"_poison_{args.poison_epochs}"
+    if args.attempt != 0:
+        model_dir += f"_attempt_{args.attempt}"
     return model_dir
 
 
-def get_mask_layers():
+def get_poison_epochs():
     args = get_args()
-    mask_layers = []
-    if args.layer != "":
-        mask_layers = [int(layer) for layer in args.layer.split(",")]
-    return mask_layers
+    poison_epochs = []
+    if args.poison_epochs != "":
+        poison_epochs = [int(epoch) for epoch in args.poison_epochs.split(",")]
+    return poison_epochs
